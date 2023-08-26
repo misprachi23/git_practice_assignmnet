0dee9a067daaf7d8eccbb66d7d405787c5ce23c6
Anshuman Chaturvedi <anshumanchaturvedi91@gmail.com>
2023-08-26 15:29:29 +0530

    code changed

---
+++ Problem1.js
@@ -0,0 +1,15 @@
+  let num = 5;
+
+  var count =0;
+for(let i=1; i<=num; i++){
+    if(number % i==0){
+        count++
+    }
+}
+ if(count == 2){
+console.log("Yes")
+}
+else{
+
+console.log("No")
+}
