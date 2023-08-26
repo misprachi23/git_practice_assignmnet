12184b5ab34166c293ce867b3b23e1b4f4043c04
Anshuman Chaturvedi <anshumanchaturvedi91@gmail.com>
2023-08-26 13:56:32 +0530

    added Problem1.js file

---
+++ Problem1.js
@@ -0,0 +1,20 @@
+let num = 7;
+
+let count = 0;
+
+for(let i = 1; i<=num; i++)
+   {
+ if( num % i == 0)
+  {
+ count++;
+  }
+}
+
+if(count == 2)
+  {
+ console.log("Prime");
+  }
+else
+  {
+console.log("Not Prime");
+  }
