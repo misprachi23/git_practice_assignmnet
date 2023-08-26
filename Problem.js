99ce923ee9d8612fe33bd81e61dafc404cc8506c
Anshuman Chaturvedi <anshumanchaturvedi91@gmail.com>
2023-08-26 16:15:30 +0530

    changes made in Problem.js file

--- Problem.js
+++ Problem.js
@@ -1,11 +1,14 @@
 let number = 5;
 
-var count =0;
-for(let i=1; i<=Number; i++){
+let count =0;
+
+for(let i=1; i<=Number; i++)
+    {
     if(number % i==0){
         count++
     }
 }
+
 if(count == 2)
    {
 console.log("Yes")
